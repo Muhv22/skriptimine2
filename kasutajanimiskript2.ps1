cb19bf73522246491cb4e739015bab63593253cf
unknown <Administrator@KRIS.VIIRA>
2022-11-16 06:09:03 -0800

    kasutaja nime skript 2

---
+++ kasutajanimiskript2.ps1
@@ -0,0 +1,15 @@
+﻿    $kasutajaparool = ConvertTo-SecureString 'Parool1!' -AsPlainText -Force
+#kasutaja loomine ees-perenimi ning tõlge nimi.perekonnanimi
+    $kasutajanimi = $Eesnimi.ToLower() + "." + $Perenimi.ToLower()
+    $täisnimi = $eesnimi.ToLower() + "." + $Perenimi
+    $kirjeldus = "Lokaalne kasutaja -" + $kasutajanimi
+    echo "Loodav kasutaja on $username"
+    Write-Output "Loodav kasutaja on $($kasutajanimi))"
+    $EroorActionPreference = 'silentlycontinue' 
+    New-LocalUser $kasutajanimi -Password $kasutajaparool -FullName $täisnimi -Description $kirjeldus
+    #kontroll
+    if(!$?){
+    Write-Output "Tekkinud on tõrge kasutaja loomisega" 
+    }
+    #Süsteemi teaded tagasi sisse
+    $ErrorActionPreference = 'stop'
